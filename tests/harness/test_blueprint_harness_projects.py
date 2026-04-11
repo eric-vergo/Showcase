37f9059d9b047f03183dad580ba26cec6092587b
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-04-11 17:27:03 +0200

    fix(references): init required reference submodules

--- tests/harness/test_blueprint_harness_projects.py
+++ tests/harness/test_blueprint_harness_projects.py
@@ -79,6 +79,22 @@ class BlueprintHarnessProjectsTests(unittest.TestCase):
         self.assertEqual(projects[1].targets[0].ref, "2ff4a3c671ab224a9db682e3ac146a23657b84e8")
         self.assertEqual(projects[1].browser_tests_path, None)
         self.assertEqual(projects[1].panel_regression_script, None)
+        self.assertEqual(
+            projects[2].prepare_command,
+            (
+                "git",
+                "-c",
+                "url.https://github.com/.insteadOf=git@github.com:",
+                "-c",
+                "url.https://github.com/.insteadOf=ssh://git@github.com/",
+                "submodule",
+                "update",
+                "--init",
+                "--depth",
+                "1",
+                "Sphere-Packing-LaTeX-Reference",
+            ),
+        )
         self.assertEqual(projects[3].repository, "https://github.com/ejgallego/verso-flt.git")
         self.assertEqual(
             projects[3].prepare_command,
@@ -99,6 +115,18 @@ class BlueprintHarnessProjectsTests(unittest.TestCase):
         self.assertEqual(projects[4].repository, "https://github.com/ejgallego/verso-algebraic-combinatorics.git")
         self.assertEqual([target.release for target in projects[4].targets], ["v4.28.0"])
         self.assertEqual(projects[4].targets[0].ref, "5a8c3b093166d7f8368fe4a610b88bac097ffc35")
+        self.assertEqual(
+            projects[4].prepare_command,
+            (
+                "git",
+                "submodule",
+                "update",
+                "--init",
+                "--depth",
+                "1",
+                "algebraic-combinatorics",
+            ),
+        )
 
     def test_reference_pages_workflow_stages_every_manifest_project(self) -> None:
         catalog = load_project_catalog(default_project_manifest(PACKAGE_ROOT))
