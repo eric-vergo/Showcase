d93c102376a616a595d77cb68921a1b5de8bbd8e
Wojciech Nawrocki <wjn@lean-fro.org>
2026-06-28 01:01:22 -0400

    feat: widgets

--- src/VersoBlueprint.lean
+++ src/VersoBlueprint.lean
@@ -57,5 +57,6 @@ import VersoBlueprint.Resolve
 import VersoBlueprint.TraversalIndex
 import VersoBlueprint.StyleSwitcher
 import VersoBlueprint.Profiling
+import VersoBlueprint.Widget
 
 set_option doc.verso true
